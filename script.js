878acf86467d89dac0b2caa2524ea71fdbf68653
Eshandwivedi13 <79198616+Eshandwivedi13@users.noreply.github.com>
2024-04-24 23:06:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-//your JS code here. If required.
+//There should be a 6 digit input where users can insert numbers and on typing the number the next input should be seen focuse
+//On backspace key press the number should be deleted from the last filled input and the input before it should be focused.
+
