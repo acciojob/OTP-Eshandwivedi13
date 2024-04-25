471a35ac84dceff9b2d39670e56e1e0a055c424d
Eshandwivedi13 <79198616+Eshandwivedi13@users.noreply.github.com>
2024-04-25 12:42:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let inputs = document.getElementsByClassName("code");
+window.onload = function() {
+    document.getElementById('code-1').focus();
+};
+
 for(let t of inputs){
 	t.addEventListener("input", inputChanged);
 	t.addEventListener("keydown", inputChangedAgain);
@@ -6,18 +10,21 @@ for(let t of inputs){
 function inputChanged(event_details){
 	let inputTarget = event_details.target;
 	let idx = parseInt(inputTarget.id.replace('code-', ''));//1based h but agle pe point kar rha h
-	setTimeout(() => inputs[idx].focus(), 200);
+	// setTimeout(() => inputs[idx].focus(), 200);
 	inputs[idx].focus();
 }
 function inputChangedAgain(event_details){
 	let key = event_details.key;
+	let inputTarget = event_details.target;
 	if(key.toLowerCase() === 'backspace'){
 		event_details.preventDefault();//backspace dabao to peeche wala uda rha tha, par cursor whi
-		let inputTarget = event_details.target;
-		inputTarget.value  = "";//peeche wala to .value se saaf kar sakte
-		let idx = parseInt(inputTarget.id.replace('code-', '')) - 2;//0 based h aur peeche point kr rha
-		setTimeout(() => inputs[idx].focus(), 200);
-		if(idx >= 0) inputs[idx].focus();
+			inputTarget.value  = "";//peeche wala to .value se saaf kar sakte
+			let idx = parseInt(inputTarget.id.replace('code-', '')) - 2;//0 based h aur peeche point kr rha
+			// setTimeout(() => inputs[idx].focus(), 200);
+			if(idx >= 0) inputs[idx].focus();
+	}
+	if(key.toLowerCase() === 'keydown'){
+		console.log("abc")
 	}
 }
 
